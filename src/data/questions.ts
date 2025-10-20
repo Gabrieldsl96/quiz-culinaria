c11fe046a5b566ae32b059d766ee6c24c0204839
Gabriel da Silva Lemos <gabrieldasilvalemos96@gmail.com>
2025-10-20 15:29:59 -0300

    primeiro commit

---
+++ src/data/questions.ts
@@ -0,0 +1,24 @@
+import { Question } from "@/types/Questions";
+
+export const questions: Question[] = [
+    {
+        question: 'Qual ingrediente não pode faltar?',
+        options: [
+            'Farinha',
+            'Corante',
+            'Água',
+            'Açúcar'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual melhor método de cozinhar?',
+        options: [
+            'Microondas',
+            'Fogão',
+            'Fogueira',
+            'Lança-chamas'
+        ],
+        answer: 3
+    }
+];
